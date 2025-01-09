88625982623b2fbf26812bc8056f507065dcc15e
gouda <dev.mohamedgouda@gmail.com>
2025-01-09 14:37:53 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[\App\Http\Controllers\WhatsappLinkController::class,'login']);
 
-Route::get('webhook/beon', function(Request $request){
-
-});
+Route::get('webhook/beon', [\App\Http\Controllers\WhatsappLinkController::class,'callback']);
